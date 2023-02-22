ebe273f4b1df7715a1183705de210e0b90381095
fernandovs4 <fervsantos3000@gmail.com>
2023-02-22 11:17:16 -0300

    adicionando asteroide

--- variaveis.py
+++ variaveis.py
@@ -2,9 +2,9 @@ import random
 import numpy as np
 from constantes import *
 raio_planeta = random.randint(20,60)
-posicao_planeta = np.array([random.randint(int(raio_planeta/2), int(1280-raio_planeta/2)), random.randint(int(raio_planeta/2), int(720-raio_planeta/2))])
+posicao_planeta = np.array([random.randint(400, int(1280-raio_planeta/2)), random.randint(int(raio_planeta/2), int(720-raio_planeta/2))])
 raio_poeira = random.randint(100,200)
-posicao_poeira = np.array([random.randint(int(raio_poeira/2), int(1280-raio_poeira/2)), random.randint(int(raio_poeira/2), int(720-raio_poeira/2))])
+posicao_poeira = np.array([random.randint(int(raio_poeira/2) + 200, int(1280-raio_poeira/2)), random.randint(int(raio_poeira/2), int(720-raio_poeira/2))])
 COR_1 = (255, 255, 255)
 COR_2 = (255, 255, 255)
 COR_3 = (255, 255, 255)

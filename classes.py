ab6b079e672e2207f996185558479ae3a49f54c0
fernandovs4 <fervsantos3000@gmail.com>
2023-02-22 21:10:07 -0300

    adicionando comentarios no código

--- classes.py
+++ classes.py
@@ -11,6 +11,7 @@ class Estrela(pygame.sprite.Sprite):
         '''Classe para criar a estrela, o objeto que disparará no alvo'''
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
+            # Carrega a imagem da estrela e redimensiona para o tamanho desejado
             self.image = pygame.image.load(path / "imagens/estrela.png")
             self.image = pygame.transform.scale(self.image, (30,30))
             
@@ -41,97 +42,126 @@ class Estrela(pygame.sprite.Sprite):
 class Alvo(pygame.sprite.Sprite):
         '''Classe para criar o alvo'''
         def __init__(self):
+            # Inicializa a classe pygame.sprite.Sprite
             pygame.sprite.Sprite.__init__(self)
+
+            # Carrega a imagem do alvo e redimensiona para o tamanho desejado
             self.image = pygame.image.load(path / "imagens/Attack_2.png")
             self.image = pygame.transform.scale(self.image, (100,100))
-          
+
+            # Define a posição inicial do alvo
             self.rect = self.image.get_rect()
             self.rect.center = np.array([1230,HEIGHT/2])
-   
-            
+
+                
         def update(self):
-            '''Método para atualizar a posição dos pássaros e trocar as imagens dos pássaros para dar animação'''
-        
-            self.rect.center = np.array([1230,random.randint(50, HEIGHT - 50) ])  
+            '''Método para atualizar a posição do alvo e trocar sua imagem para criar animação'''
+
+            # Atualiza a posição do alvo com uma posição aleatória na vertical
+            self.rect.center = np.array([1230,random.randint(50, HEIGHT - 50) ])
     
 class Planeta(pygame.sprite.Sprite):
         '''Classe para alterar a velocidade de lançamento dos projéteis que a estrela arremessa'''
         def __init__(self,raio, posicao):
             pygame.sprite.Sprite.__init__(self)
+
+            # Carrega a imagem do planeta e ajusta seu tamanho
             self.image = pygame.image.load(path / f"imagens/planeta.png")
             self.image = pygame.transform.scale(self.image, (raio,raio))
 
+            # Define a posição do planeta na tela
             self.rect = self.image.get_rect()
             self.rect.centerx = posicao[0]
             self.rect.centery = posicao[1]
-    
+            
         def update(self, state):
+            # Calcula o vetor da estrela ao planeta e a direção da gravidade
             vetor_planeta_estrela = np.array([self.rect.centerx, self.rect.centery]) - np.array([state['estrela'].rect.centerx, state['estrela'].rect.centery])
             direcao_gravidade = vetor_planeta_estrela / np.linalg.norm(vetor_planeta_estrela)
+            
+            # Calcula a força gravitacional do planeta sobre a estrela
             DT = 100000/np.linalg.norm(vetor_planeta_estrela)**2
             gravidade = 0.2*DT * direcao_gravidade
 
+            # Aplica a força gravitacional na velocidade da estrela
             if pygame.sprite.spritecollide(state['estrela'], state['poeiras'], False) :
-                state['velocidade']   = state['velocidade']*0.99 +  gravidade
+                state['velocidade'] = state['velocidade']*0.99 +  gravidade
             else:
-                state['velocidade']  = state['velocidade'] +  gravidade
+                state['velocidade'] = state['velocidade'] +  gravidade
+
             
-            
+             # Atualiza a posição da estrela de acordo com sua velocidade
             state['velocidade']  = state['velocidade'] +  gravidade
             state['estrela'].rect.centerx = state['estrela'].rect.centerx +  0.1*state['velocidade'][0]
             state['estrela'].rect.centery = state['estrela'].rect.centery +  0.1*state['velocidade'][1]
+                    
+            
+            
+           
 
 
 class Poeira(pygame.sprite.Sprite):
         '''Classe para alterar as propriedades das poeiras'''
         def __init__(self,largura,altura, posicao):
             pygame.sprite.Sprite.__init__(self)
+            # Carrega a imagem da poeira e ajusta o tamanho
             self.image = pygame.image.load(path / "imagens/poeira.png")
-            self.image = pygame.transform.scale(self.image, (largura,altura))
+            self.image = pygame.transform.scale(self.image, (largura, altura))
 
+            # Define a posição inicial da poeira
             self.rect = self.image.get_rect()
             self.rect.centerx = posicao[0]
             self.rect.centery = posicao[1]
-           
- 
-
             
         def update(self):
+            # Define uma nova posição aleatória para a poeira
             self.rect.center = np.array([random.randint(int(variaveis.raio_poeira/2), int(1280-variaveis.raio_poeira/2)), random.randint(int(variaveis.raio_poeira/2), int(720-variaveis.raio_poeira/2))])
-         
-
-                
-    
-
+                        
+            
 class Tela_inicial(pygame.sprite.Sprite):
     '''Classe para criar a tela inicial'''
-    def __init__(self):
-            pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.image.load(path / 'imagens/wallpaper_estrelas.jpeg')
-            self.rect = self.image.get_rect()
-    
 
+    def __init__(self):
+        # Inicializa a superclasse
+        pygame.sprite.Sprite.__init__(self)
+
+        # Carrega a imagem de fundo
+        self.image = pygame.image.load(path / 'imagens/wallpaper_estrelas.jpeg')
+
+        # Obtém o retângulo que circunscreve a imagem
+        self.rect = self.image.get_rect()
+
+    
 
 
 
 class Help(pygame.sprite.Sprite):
-    '''Classe para criar a tela de ajuda, onde fica explicado os níveis do jogo'''
+    '''
+    Classe para criar a tela de ajuda, onde é explicado os níveis do jogo
+    '''
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        
+        # Carrega a imagem de fundo da tela de ajuda
         self.image = pygame.image.load(path / 'imagens/wallpaper_estrelas.jpeg')
+        
+        # Define o retângulo da imagem
         self.rect = self.image.get_rect()
 
 class Asteroide(pygame.sprite.Sprite):
+    '''Classe para criar os asteroides e movê-los em torno do planeta'''
     def __init__(self, posicao):
         pygame.sprite.Sprite.__init__(self)
+        # Carregando a imagem do asteroide e escalonando
         self.image = pygame.image.load(path / 'imagens/asteroide.png')
         self.image = pygame.transform.scale(self.image, (5,5))
+        # Definindo a posição do asteroide
         self.rect = self.image.get_rect()
         self.rect.centerx = posicao[0] + 200
         self.rect.centery = posicao[1] 
 
     def update(self, posicao, angle):
-        
+        # Atualizando a posição do asteroide de acordo com o ângulo e a posição do planeta
         self.rect.centerx = posicao[0]  + 100*math.cos(math.radians(angle)) 
         self.rect.centery = posicao[1] + 100*math.sin(math.radians(angle))
         
--- main.py
+++ main.py
@@ -44,22 +44,25 @@ def inicializa():
     poeiras = pygame.sprite.Group()
     poeiras.add(poeira)
 
+    #Define a fonte de texto com tamanho 40
     font3 = pygame.font.Font(None,40)
     
-
+    # Renderiza o texto "Game Over" na cor vermelha (RED) utilizando a fonte definida anteriormente
     text = font3.render('Game Over', True, RED)
+    #Define as posições e dimensões dos retângulos para os botões da tela de game over
     screen_help_rect = pygame.Rect(361, 7, 63, 31)
     principal_menu_rect = pygame.Rect(74, 396, 252, 59)
     play_again_rect = pygame.Rect(74, 321, 252, 59)
     exit_rect = pygame.Rect(74, 471, 252, 59)
+    #Define a posição central do retângulo que contém o texto "Game Over"
     text_rect = text.get_rect(center = (20,20))
 
 
-
+    # Cria uma instância da classe Tela_inicial e adiciona a tela em um grupo de telas iniciais
     tela_inicial = Tela_inicial()
     telas_iniciais = pygame.sprite.Group()
     telas_iniciais.add(tela_inicial)
-
+    #Cria um grupo que contém a tela de ajuda e adiciona essa tela ao grupo
     all_help_screen = pygame.sprite.Group()
     help_screen = Help()
     all_help_screen.add(help_screen)
@@ -94,40 +97,49 @@ def finaliza():
 angle = 0
 def desenha(window: pygame.Surface, assets, state):
 
+    
+    # Define as cores globais para serem utilizadas na tela inicial
     global COR_1
     global COR_2
     global COR_3
 
-
     if state['tela_inicial']:
-        '''Desenha a tela inicial do jogo'''
+        # Desenha a tela inicial do jogo
         window.blit(assets['fundo_inicio'], (0,0))
+        
+        # Define as fontes utilizadas na tela inicial
         fonte = pygame.font.SysFont('Arial', 40)
         fonte_pontos = pygame.font.SysFont('Arial', 16)
+
+        # Renderiza os textos da tela inicial
         iniciar_jogo = fonte.render('Iniciar jogo', True, COR_1)
         sair = fonte.render('Sair', True, COR_2)
         instrucoes = fonte.render('Instruções', True, COR_3)
         pontos_record = fonte_pontos.render('Record: {}'.format(state['record']), True, (255, 255, 255))
 
+        # Obtém as posições dos textos
         posicao_iniciar_jogo = iniciar_jogo.get_rect()
         posicao_sair = sair.get_rect()
         posicao_instrucoes = instrucoes.get_rect()
         posicao_record = pontos_record.get_rect()
 
-
+        # Define as posições dos textos
         posicao_iniciar_jogo.topright = (700, 300)
         posicao_sair.topright = (645, 500)
         posicao_instrucoes.topright = (695, 400)
         posicao_record.topright = (1270, 0)
 
+        # Desenha os textos na tela
         window.blit(iniciar_jogo, posicao_iniciar_jogo)
         window.blit(instrucoes, posicao_instrucoes)
         window.blit(sair, posicao_sair)
         window.blit(pontos_record, posicao_record)
 
+        # Verifica se o mouse está sobre um dos botões e altera a cor do texto
         if posicao_iniciar_jogo.collidepoint(pygame.mouse.get_pos()):
             COR_1 = (255, 0, 0)
             if pygame.mouse.get_pressed()[0]:
+                # Inicia o jogo
                 state['tela_inicial'] = False
                 state['tela_jogo'] = True
         else:
@@ -136,6 +148,8 @@ def desenha(window: pygame.Surface, assets, state):
         if posicao_sair.collidepoint(pygame.mouse.get_pos()):
             COR_2 = (255, 0, 0)
             if pygame.mouse.get_pressed()[0]:
+                # Finaliza o jogo
+                
                 finaliza()
         else:
             COR_2 = (255, 255, 255)
@@ -143,29 +157,43 @@ def desenha(window: pygame.Surface, assets, state):
         if posicao_instrucoes.collidepoint(pygame.mouse.get_pos()):
             COR_3 = (255, 0, 0)
             if pygame.mouse.get_pressed()[0]:
+                # Abre a tela de instruções
                 state['tela_inicial'] = False
                 state['tela_instrucoes'] = True
         else:
             COR_3 = (255, 255, 255)
+
         
 
         
     if state['tela_instrucoes']:
-        '''Desenha a tela de instruções do jogo'''
+        
+        # Define a fonte e tamanho da fonte para o texto 'Voltar'
         fonte = pygame.font.SysFont('Arial', 40)
+
+        # Renderiza o texto 'Voltar' com a fonte definida
         voltar = fonte.render('Voltar', True, (0,0,0))
+
+        # Obtém as dimensões da imagem de 'Voltar' renderizada
         posicao_voltar = voltar.get_rect()
+
+        # Define a posição da imagem de 'Voltar' no canto superior direito da tela
         posicao_voltar.topright = (700, 600)
+
+        # Desenha a imagem de fundo da tela de instruções na janela
         window.blit(assets['fundo_instrucoes'], (0,0))
+
+        # Desenha a imagem de 'Voltar' na janela na posição definida
         window.blit(voltar, posicao_voltar)
-        
-        
-        
+
+        # Verifica se o mouse está sobre a imagem de 'Voltar'
         if posicao_voltar.collidepoint(pygame.mouse.get_pos()):
+            # Verifica se o botão esquerdo do mouse está pressionado
             if pygame.mouse.get_pressed()[0]:
+                # Define as variáveis de estado para retornar à tela inicial
                 state['tela_inicial'] = True
                 state['tela_instrucoes'] = False
-  
+
         
         
     if state['tela_jogo']:
@@ -173,35 +201,47 @@ def desenha(window: pygame.Surface, assets, state):
 
         clock.tick(FPS)
         '''Função utilizada para desenhar todos os sprites na tela'''
+        # Define a cor de fundo da tela e desenha o fundo do jogo
         window.fill((0,0,0))
         window.blit(assets['fundo'], (0,0))
+
+        # Desenha o score do jogador
         fonte_pontos = pygame.font.SysFont('Arial', 16)
         pontos_texto = fonte_pontos.render('Score: {}'.format(state['pontos']), True, (255, 255, 255))
         posicao_texto = pontos_texto.get_rect()
         posicao_texto.topright = (1270, 0)
         window.blit(pontos_texto, posicao_texto)
-      
+
+        # Desenha uma linha caso o jogador esteja arrastando uma estrela
         if state['arrastando'] == True:
             pygame.draw.line(window, (255, 255, 255), posicao_inicial_estrela, pygame.mouse.get_pos(), 1)
-            
+
+        # Desenha todos os sprites na tela
         state['estrelas'].draw(window)
-        
         state['alvos'].draw(window)
         state['planetas'].draw(window)
         state['poeiras'].draw(window)
         state['asteroides'].draw(window)
+
+        # Desenha uma linha colorida quando o jogador arrasta uma estrela
        
-   
+        
         if state['arrastando'] == True:
+            #Desenha uma linha de cor que varia entre verde e vermelho com base na distância entre a posição inicial da estrela arrastada e a posição atual do mouse
+            #A cor da linha é determinada pelo valor mínimo entre a distância e 255
             color_line = min(((pygame.mouse.get_pos()[0]  - posicao_inicial_estrela[0])**2  + (pygame.mouse.get_pos()[1]  - posicao_inicial_estrela[1] )**2)**0.5, 255)
 
             pygame.draw.line(window, (0 + color_line, 255 - color_line , 0), posicao_inicial_estrela, pygame.mouse.get_pos(), 2)
         
-
+    #Gera novos planetas se a pontuação dividida pela fase atual for maior do que 5 e o número de planetas for menor do que 3
     if state['pontos']/state['fase'] > 5 and len(state['planeta']) < 3 :
+        #Gera um novo planeta com tamanho entre 20 e 50 pixels e posição aleatória na tela
         planeta = gera_planeta(random.randint(20, 50), np.array([random.randint(400, 1100), random.randint(100, 650)]))
+        #Adiciona o planeta na lista de planetas do jogo
         state['planeta'].append(planeta)
+        #Adiciona o planeta ao grupo de sprites de planetas
         state['planetas'].add(planeta)
+        #Incrementa a fase atual do jogo
         state['fase'] += 1
 
 
@@ -213,37 +253,51 @@ def atualiza_estado(state):
     global angle
     '''Função utilizada para atualizar os estados do jogo'''
   
+    # Verifica eventos do Pygame
     for ev in pygame.event.get():
+        # Verifica se o usuário clicou no botão "X" para fechar a janela
         if ev.type == pygame.QUIT:
+            # Se o estado atual tiver mais pontos do que o recorde, atualiza o recorde
             if state['pontos'] > int(state['record']):
-            # Abre o arquivo em modo de escrita e escreve a nova pontuação
+                # Abre o arquivo em modo de escrita e escreve a nova pontuação
                 with open(path / 'record.txt', 'w') as file:
                     file.write(str(state['pontos']))
-                # Atualiza o recorde com a nova pontuação
+                    # Atualiza o recorde com a nova pontuação
                     state['record'] = state['pontos']
+            # Retorna "False" para indicar que o jogo deve ser encerrado
             return False
         
-      
+        # Verifica se o usuário clicou com o botão esquerdo do mouse
         posicao_mouse = np.array(pygame.mouse.get_pos())
         if ev.type == pygame.MOUSEBUTTONDOWN:
             if ev.button == 1:
+                # Verifica se o jogo está em andamento
                 if state['em_andamento'] == False:
+                    # Verifica se o mouse está sobre a estrela
                     if state['estrela'].rect.collidepoint(ev.pos):
+                        # Move a estrela para a posição atual do mouse e marca que está sendo arrastada
                         state['estrela'].rect.center = pygame.mouse.get_pos()
                         state['arrastando'] = True
+        # Verifica se o usuário soltou o botão esquerdo do mouse
         elif ev.type == pygame.MOUSEBUTTONUP:
             if ev.button == 1:
+                # Verifica se a estrela está sendo arrastada
                 if state['arrastando'] == True:
+                    # Calcula a velocidade com que a estrela deve ser lançada
                     velocidade =  posicao_inicial_estrela - posicao_mouse
                     modulo_velocidade= (velocidade[0]**2 + velocidade[1]**2)**0.5
                     unitario = velocidade/modulo_velocidade
                     if modulo_velocidade> 100:
                         velocidade = unitario*100
                     modulo_velocidade= (velocidade[0]**2 + velocidade[1]**2)**0.5
+                    # Armazena a velocidade no estado atual do jogo e atualiza a posição da estrela
                     state['velocidade'] = velocidade
                     state['estrela'].update(state['velocidade'], state['atingiu'])
+                    # Marca que o jogo está em andamento
                     state['em_andamento'] = True
+                # Marca que a estrela não está mais sendo arrastada
                 state['arrastando'] = False
+        # Se o usuário está arrastando a estrela, atualiza a posição dela para a posição atual do mouse
         elif state['arrastando'] == True:
             state['estrela'].rect.center = pygame.mouse.get_pos()
     
@@ -251,43 +305,54 @@ def atualiza_estado(state):
                     
                         
               
-    if pygame.sprite.spritecollide(state['estrela'], state['alvos'], False) :
-        state['pontos']+= 1
+    # Verifica se houve colisão entre a estrela e os alvos
+    if pygame.sprite.spritecollide(state['estrela'], state['alvos'], False):
+        # Adiciona um ponto
+        state['pontos'] += 1
+        # Atualiza a posição do alvo
         state['alvo'].update()
+        # Atualiza a posição da poeira
         state['poeira'].update()
+        # Zera a velocidade
         state['velocidade'] *= 0
-        state['estrela'].update(state['velocidade'],True)
+        # Atualiza a posição da estrela
+        state['estrela'].update(state['velocidade'], True)
+        # Define que o jogo não está mais em andamento
         state['em_andamento'] = False
-        
-    
+
+    # Verifica se a estrela passou da tela
     passou_da_tela = state['estrela'].update(state['velocidade'], False)
 
+    # Verifica se o jogo está em andamento
     if state['em_andamento']:
-            for i in range(state['fase']):
-                state['planeta'][i].update(state)
+        # Atualiza a posição dos planetas
+        for i in range(state['fase']):
+            state['planeta'][i].update(state)
 
+    # Atualiza a posição do asteroide
     state['asteroide'].update(posicao_planeta, angle)
-    
+
+    # Incrementa o ângulo
     angle += 1
-       
 
-    
-
-    # state['planeta'].update(state['velocidade'],np.array([state['estrela_obj'].rect.centerx, state['estrela_obj'].rect.centery]), state['estrela_obj'])
+    # Verifica se a estrela passou da tela
     if passou_da_tela:
+        # Zera a velocidade
         state['velocidade'] *= 0
+        # Define que o jogo não está mais em andamento
         state['em_andamento'] = False
 
+        # Verifica se a pontuação atual é maior que a pontuação recorde
         if state['pontos'] > int(state['record']):
-            # Abre o arquivo em modo de escrita e escreve a nova pontuação
+            # Abre o arquivo de recorde em modo de escrita
             with open(path / 'record.txt', 'w') as file:
+                # Escreve a nova pontuação no arquivo
                 file.write(str(state['pontos']))
             # Atualiza o recorde com a nova pontuação
-                state['record'] = state['pontos']
+            state['record'] = state['pontos']
+
+        # Zera a pontuação atual
         state['pontos'] = 0
-   
-
-
     return True
 
 def gameloop(window, assets, state):
